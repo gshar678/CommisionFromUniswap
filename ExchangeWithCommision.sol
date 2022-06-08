9732c8551d6acc52aad4194782b4cfa0d54f378a
gshar678 <107056430+gshar678@users.noreply.github.com>
2022-06-09 00:27:41 +0530

    Update ExchangeWithCommision.sol

--- ExchangeWithCommision.sol
+++ ExchangeWithCommision.sol
@@ -1,60 +1,134 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ISwap {
 
-  function swapExactTokensForTokens( 
-  uint amountIn,
-  uint amountOutMin,
-  address[] calldata path,
-  address to,
-  uint deadline) external view returns (uint[] memory amounts);
+  function getAmountsOut(uint amountIn, address[] memory path)
+    external
+    view
+    returns (uint[] memory amounts);
 
-  function swapTokensForExactTokens( uint amountOut,
-  uint amountInMax,
-  address[] calldata path,
-  address to,
-  uint deadline) external view returns (uint[] memory amounts);
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactTokensForETH(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactETHForTokens(
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external payable returns (uint[] memory amounts);
+
+  function addLiquidity(
+    address tokenA,
+    address tokenB,
+    uint amountADesired,
+    uint amountBDesired,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  )
+    external
+    returns (
+      uint amountA,
+      uint amountB,
+      uint liquidity
+    );
+
+  function removeLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB);
+}
+
+interface IUniswapV2Pair {
+  function token0() external view returns (address);
+
+  function token1() external view returns (address);
+
+  function getReserves()
+    external
+    view
+    returns (
+      uint112 reserve0,
+      uint112 reserve1,
+      uint32 blockTimestampLast
+    );
+
+  function swap(
+    uint amount0Out,
+    uint amount1Out,
+    address to,
+    bytes calldata data
+  ) external;
+
 
 }
 
-contract ExchangeWithCommision {
+
+contract ExchangeWithCommision  {
 
   address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+  address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
 
   function swapExactTokensForTokensWithCommision ( 
+  address tokenIn,  
+  address tokenOut,  
   uint amountIn,
   uint amountOutMin,
   address[] calldata path,
   address to,
-  uint deadline) external payable
+  uint deadline) external 
  
  {
 
-    require(msg.value == amountIn);
+    IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+    IERC20(tokenIn).approve(UNISWAP_V2_ROUTER, amountIn);
 
-    payable(address(this)).transfer(amountOutMin*1/100);   
+    address[] memory path;
+    if (tokenIn == WETH || tokenOut == WETH) {
+      path = new address[](2);
+      path[0] = tokenIn;
+      path[1] = tokenOut;
+    } else {
+      path = new address[](3);
+      path[0] = tokenIn;
+      path[1] = WETH;
+      path[2] = tokenOut;
+    }
 
-    ISwap(UNISWAP_V2_ROUTER).swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
- }
+    uint amountInDeducted = (amountIn * 99)/100;
 
-  
- function swapTokensForExactTokensWithCommision (
-  uint amountOut,
-  uint amountInMax,
-  address[] calldata path,
-  address to,
-  uint deadline
+    ISwap(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
+      amountInDeducted,
+      amountOutMin,
+      path,
+      to,
+      block.timestamp
+    );
 
-  ) external payable
- 
- {
-       require(msg.value == amountInMax);
-
-       payable(address(this)).transfer(amountOut*1/100);
-
-       ISwap(UNISWAP_V2_ROUTER).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
- }
+   
+  }
 
 
  }
