7977fcb90a9f28678fd0fe066e79cc9c015e41b9
gshar678 <107056430+gshar678@users.noreply.github.com>
2022-06-09 00:33:30 +0530

    Update ExchangeWithCommision.sol
    ERC20 token swap functionality added.

--- ExchangeWithCommision.sol
+++ ExchangeWithCommision.sol
@@ -60,34 +60,11 @@ interface ISwap {
   ) external returns (uint amountA, uint amountB);
 }
 
-interface IUniswapV2Pair {
-  function token0() external view returns (address);
-
-  function token1() external view returns (address);
-
-  function getReserves()
-    external
-    view
-    returns (
-      uint112 reserve0,
-      uint112 reserve1,
-      uint32 blockTimestampLast
-    );
-
-  function swap(
-    uint amount0Out,
-    uint amount1Out,
-    address to,
-    bytes calldata data
-  ) external;
-
-
-}
 
 
 contract ExchangeWithCommision  {
 
-  address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+  address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
   address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
 
@@ -103,7 +80,7 @@ contract ExchangeWithCommision  {
  {
 
     IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
-    IERC20(tokenIn).approve(UNISWAP_V2_ROUTER, amountIn);
+    IERC20(tokenIn).approve(ROUTER, amountIn);
 
     address[] memory path;
     if (tokenIn == WETH || tokenOut == WETH) {
@@ -119,16 +96,14 @@ contract ExchangeWithCommision  {
 
     uint amountInDeducted = (amountIn * 99)/100;
 
-    ISwap(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
+    ISwap(ROUTER).swapExactTokensForTokens(
       amountInDeducted,
       amountOutMin,
       path,
       to,
       block.timestamp
     );
-
    
   }
-
 
  }
